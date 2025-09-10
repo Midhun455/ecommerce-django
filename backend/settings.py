21c707ad9eb1e8fd47192f4ba47b8ba38138b55f
Midhun Suresh <84007961+Midhun455@users.noreply.github.com>
2025-09-10 15:51:11 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -175,9 +175,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "your@gmail.com"
-EMAIL_HOST_PASSWORD = "APP_PASSWORD"
-DEFAULT_FROM_EMAIL = "your@gmail.com"
+EMAIL_HOST_USER = "midhuneasybis@gmail.com"
+EMAIL_HOST_PASSWORD = "ounrlpzgvhdfukns"
+DEFAULT_FROM_EMAIL = "midhuneasybis@gmail.com"
 
 
 STATIC_URL = "/static/"
